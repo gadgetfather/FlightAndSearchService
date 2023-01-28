8233d81dc3a780b3d10f63b3554500f91e46fba8
Akshay <kamblea635@gmail.com>
2023-01-28 13:34:21 +0530

    add city-controller

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,59 @@
+const { CityService, CityService } = require('../services/index')
+const CityMaker = new CityService()
+const create = async (req, res) => {
+    try {
+        const city = await CityMaker.createCity(req.body)
+        return res.status(201).json({ data: city, success: true, message: 'Created City', err: {} })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to create city',
+            err: error
+        })
+    }
+}
+const destroy = async (req, res) => {
+    try {
+        const city = await CityMaker.deleteCity(req.params.id)
+        return res.status(200).json({ data: city, success: true, message: 'Created City', err: {} })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to delete city',
+            err: error
+        })
+    }
+}
+
+const get = async (req, res) => {
+    try {
+        const city = await CityMaker.getCity(req.params.id)
+        return res.status(201).json({ data: city, success: true, message: 'Created City', err: {} })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to get city',
+            err: error
+        })
+    }
+}
+const update = async (req, res) => {
+    try {
+        const city = await CityMaker.updateCity(req.params.id,req.body)
+        return res.status(201).json({ data: city, success: true, message: 'Created City', err: {} })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to update city',
+            err: error
+        })
+    }
+}
