ed06c6e87d766cea22af1edd23522a0561ad9549
Akshay <kamblea635@gmail.com>
2023-01-29 15:48:32 +0530

    db sync

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const {PORT} =  require('./config/serverConfig')
 const ApiRoutes = require('./routes/index')
+const db = require('./models')
 const setupAndStartServer= async ()=> {
 // create express object
 const app = express()
@@ -10,7 +11,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/api',ApiRoutes)
 app.listen(PORT,()=>{
+    // add a key in .env to sync this
+    if(process.env.SYNC_DB){
+        db.sequelize.sync({alter:true})
+    }
     console.log(`running on ${PORT}`);
+    
     console.log(process.env.PORT)
 })
 }
